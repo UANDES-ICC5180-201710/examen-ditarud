bd79eb1ad5c89a9ad97e932ce0397bcb1470f32e
ditarud <ditarud@miuandes.cl>
2017-06-21 12:17:01 -0400

    Examen

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  resources :purchases
+  resources :games
+  root to: 'games#index'
+
+
+  devise_for :users, path: 'auth'
+  devise_scope :user do
+    get 'auth', to: 'devise/registrations#new'
+  end
+  authenticate :user do
   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
 end
